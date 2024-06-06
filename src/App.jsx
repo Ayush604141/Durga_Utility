16ce5dc25b5c5cb0a8ca6ff46d2f467a717b895a
Ayush Shrivastav <ayush41red@gmail.com>
2024-06-06 16:26:03 +0530

    Implement routing for the components.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import Home from "./component/Home";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import ProductsList from "./component/ProductsList";
+import products from "../data.json";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/products/list" element={<ProductsList />} />
+      <Route
+        path="/products/list"
+        element={<ProductsList products={products} />}
+      />
     </Routes>
   );
 };
